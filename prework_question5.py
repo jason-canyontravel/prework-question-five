2b55b7044509c9329663964d23351d16c17ed42b
Jason <jj.hernandez3333@gmail.com>
2022-08-12 06:08:38 -0500

    first commit

---
+++ prework_question5.py
@@ -0,0 +1,6 @@
+minimum = int(input(" Please Enter the Minimum Value : "))
+maximum = int(input(" Please Enter the Maximum Value : "))
+
+for number in range(minimum, maximum+1):
+    if(number % 2 != 0):
+        print("{0}".format(number))
